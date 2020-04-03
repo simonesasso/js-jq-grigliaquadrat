66d26e3b7949625bac18a3e503318468b3ed678e
simone <sasso.96@libero.it>
2020-04-03 15:32:07 +0200

    griglia-auto

---
+++ main.js
@@ -0,0 +1,71 @@
+// Creare in HTML una griglia formata da 8x8 rettangolini tutti bianchi.
+// 15 di questi rettangolini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+
+
+$(document).ready(function(){
+
+  // tenere il punteggio dei Verdi VS Rossi
+var redPoint = 0, greenPoint = 0;
+
+  // faccio cose
+  // $('.square').click(
+  //   function () {
+  //     if($(this).hasClass('redWannabe')){
+  //       $(this).css('background', 'red');
+  //       redPoint++;
+  //       console.log("punteggio rosso: " + redPoint);
+  //       $('.prossa').html("punteggio rosso: " + redPoint);
+  //     } else {
+  //       $(this).css('background', 'green');
+  //       greenPoint++;
+  //       console.log("punteggio verde: " + greenPoint);
+  //       $('.pverde').html("punteggio verde: " + greenPoint);
+  //     }
+  //   }
+  // );
+
+
+  $('.square').click(
+    function () {
+      if($(this).hasClass('active')){
+        console.log("OH! l'hai già cliccato!");
+      } else if($(this).hasClass('redWannabe')){
+        $(this).css('background', 'red');
+        redPoint++;
+        $(this).addClass('active');
+        // $(this).attr('active', 'vero');
+        console.log("punteggio rosso: " + redPoint);
+        $('.prossa').html("punteggio rosso: " + redPoint);
+      } else {
+        $(this).css('background', 'green');
+        greenPoint++;
+        $(this).addClass('active');
+        console.log("punteggio verde: " + greenPoint);
+        $('.pverde').html("punteggio verde: " + greenPoint);
+      }
+    }
+  );
+
+// genero un array di quindici numeri random con un while loop
+ var arrayNumRandom = [];
+
+ var i = 0;
+ while (i < 15) {
+   var numRandom = Math.floor(Math.random() * 64) + 1;
+   if (arrayNumRandom.includes(numRandom)) {
+    // se il numero è gia presente nellarray non lo includo e non incremento i
+   }else {
+     arrayNumRandom.push(numRandom);
+     i++;
+   }
+   console.log(arrayNumRandom);
+ }
+// il for attraversa larray e con eq(test) assegno  a 15 square scelti randomicamente
+// in base ai numeri generati la classe redWannabe
+ for (var j = 0; j < arrayNumRandom.length; j++) {
+   var test = arrayNumRandom[j];
+   $(".square").eq(test).addClass("redWannabe");
+ }
+
+
+});
